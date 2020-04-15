57da5a26ee647ec52df33232a6acba41bbecaa8d
MaryHooker <mc020791@yahoo.com>
2020-04-15 08:46:16 -0500

    checked

--- routes/api.js
+++ routes/api.js
@@ -17,23 +17,32 @@ router.post('/',(req,res)=>{
 router.get('/:videoTitle',(req,res)=>{
     // res.send(`Found new video by title`);
     EntryCollection.findOne({videoTitle: req.params.videoTitle}, (errors,results)=>{
-        errors ? res.send(errors) : res.send(results);
+        errors ? res.send(errors) : res.send(`Video ${req.params.videoTitle} was deleted`);
     })
 })
 
 //Find all videos
 router.get('/',(req,res)=>{
     // res.send(`Found all new videos`);
+    EntryCollection.find({}, (errors,results)=>{
+        errors ? res.send(errors) : res.send(results);
+    })
 })
 
 //Find a video by title and Update it
 router.put('/:videoTitle',(req,res)=>{
     // res.send(`Updated video`);
+    EntryCollection.findOneAndUpdate({videoTitle: req.params.videoTitle}, req.body, (errors,results)=>{
+        errors ? res.send(errors) : res.send(results);
+    })
 })
 
 //Find video by title and delete it
 router.delete('/:videoTitle',(req,res)=>{
     // res.send(`Deleted video`);
+    EntryCollection.findOneAndDelete({videoTitle: req.params.videoTitle}, (errors,results)=>{
+        errors ? res.send(errors) : res.send(results);
+    })
 })
 
 //export route
